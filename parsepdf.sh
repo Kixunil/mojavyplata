cb114bf3456e8e364d13156c9847f07229382ed7
Tomas Corej <tomas.corej@websupport.sk>
2015-08-08 13:43:12 +0200

    make sure that file exists on given path

--- parsepdf.sh
+++ parsepdf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-PAYROLLDIR=/op
 
 if [ -z "$1" ]; then
 	echo "Usage: $0 <pdffile>"
@@ -7,4 +6,6 @@ if [ -z "$1" ]; then
 fi
 payroll=$1
 
-pdftotext -raw $payroll – | sed -r -e ‘s/[0-9] – ([a-zA-Z].+)/\n\1/g’ -e ‘s/[0-9] [A-Z]/\n/g’ | parsers/hour_software.awk
+if [ -f "$payroll" ]; then 
+	pdftotext -raw "$payroll" – | sed -r -e ‘s/[0-9] – ([a-zA-Z].+)/\n\1/g’ -e ‘s/[0-9] [A-Z]/\n/g’ | parsers/hour_software.awk
+fi
