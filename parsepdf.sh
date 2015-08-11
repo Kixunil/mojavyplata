b80e4847b34a6dd814fbdcb194b9e3733c141a13
Tomas Corej <tomas.corej@websupport.sk>
2015-08-11 20:42:56 +0200

    fix weird character

--- parsepdf.sh
+++ parsepdf.sh
@@ -7,5 +7,5 @@ fi
 payroll=$1
 
 if [ -f "$payroll" ]; then 
-	/usr/bin/pdftotext -raw "$payroll" – | sed -r -e 's/[0-9] – ([a-zA-Z].+)/\n\1/g' -e 's/[0-9] [A-Z]/\n/g' | parsers/hour_software.awk
+	/usr/bin/pdftotext -raw "$payroll" - | sed -r -e 's/[0-9] - ([a-zA-Z].+)/\n\1/g' -e 's/[0-9] [A-Z]/\n/g' | parsers/hour_software.awk
 fi
